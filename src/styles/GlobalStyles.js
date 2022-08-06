e1d0f03774fec1d3a0a846eace72ca6c0e062b3f
Jorge Atempa <atempaitm@gmail.com>
2022-08-06 00:13:59 -0700

    Center content

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,12 +1,21 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
   :root {
     font-family: helvetica, arial, sans-serif;
   }
 
   body {
     background-color: blue;
+    width: 90%;
+    max-width: 1200px;
+    margin: 2em auto;
   }
 `
 
