9af51c5686fd3bfbda69f639fa5812cb81fd76dd
Jorge Atempa <atempaitm@gmail.com>
2022-08-06 16:02:30 -0700

    Update configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+  ],
 }
