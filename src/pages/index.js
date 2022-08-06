d58effc52ac8a9a05a281f76b0b632e4b5a3db96
Jorge Atempa <atempaitm@gmail.com>
2022-08-06 16:23:11 -0700

    Populate with fake data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import ProfileCard from "../components/ProfileCard"
+import people from "../components/people.json"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      {people.map(profile => (
+        <ProfileCard
+          key={profile.id}
+          firstName={profile.first_name}
+          lastName={profile.last_name}
+          email={profile.email}
+        />
+      ))}
+    </div>
+  )
 }
