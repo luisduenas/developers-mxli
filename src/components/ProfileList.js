fa51a3b7e84e8c75868757b2216f46eaf4f54af1
Jorge Atempa <atempaitm@gmail.com>
2022-08-07 17:34:39 -0700

    Create default messages for no results

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -2,6 +2,13 @@ import React from "react"
 import Profile from "./Profile"
 import styled from "styled-components"
 
+const NoResults = styled.div`
+  height: 75vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 const ProfileListContainer = styled.div`
   display: grid;
   gap: 25px;
@@ -17,6 +24,14 @@ const ProfileListContainer = styled.div`
 `
 
 const ProfileList = ({ profiles }) => {
+  if (profiles.length === 0) {
+    return (
+      <NoResults>
+        <h1>There is no results for your search!</h1>
+      </NoResults>
+    )
+  }
+
   return (
     <ProfileListContainer>
       {profiles.map(profile => (
