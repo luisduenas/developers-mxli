73eada9788ab039ff291231f9c8f1ae294aa688c
Jorge Atempa <atempaitm@gmail.com>
2022-08-07 21:11:27 -0700

    Create 'NoResults' component

---
+++ src/components/NoResults.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const NoResults = styled.div`
+  min-height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default NoResults
